792dd3c869d76afa23a00525484258ea47e99ab7
“Alejandra” <A302367@santandertecnologia.com.ar>
2020-09-15 08:24:46 -0300

    Rest Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // ==================
 process.env.PORT = process.env.PORT || 3000;
+
+// ================================
+// Entorno
+// ================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ================================
+// Base de dAtos
+// ================================
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://A302367:eDGtPN1V10ojN2Ok@cluster0.rha88.mongodb.net/cafe'
+    // }
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser')
 
@@ -9,31 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get Usuario');
-})
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
+app.use(require('./routers/usuario'));
+console.log("Conecta a la base");
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log("Base de Datos ONLINE");
     });
-})
-app.delete('/usuario', function(req, res) {
-    res.json('Delete Usuario');
-})
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', 3000);
 })
