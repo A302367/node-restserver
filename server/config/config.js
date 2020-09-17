6d6048f728fd6e71160228556a17b47e9d47b2ce
“Alejandra” <A302367@santandertecnologia.com.ar>
2020-09-17 10:01:29 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -14,10 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://A302367:eDGtPN1V10ojN2Ok@cluster0.rha88.mongodb.net/cafe'
-    // }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://A302367:eDGtPN1V10ojN2Ok@cluster0.rha88.mongodb.net/cafe'
+}
 
 process.env.URLDB = urlDB;
