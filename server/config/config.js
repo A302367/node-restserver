3e1498ff4d3059910de495eadd3a16854e628904
“Alejandra” <A302367@santandertecnologia.com.ar>
2020-09-18 09:33:57 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://A302367:eDGtPN1V10ojN2Ok@cluster0.rha88.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
